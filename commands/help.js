f0e8ce7b8b92c34b6150694dd91aed3177d7c9dd
Vitgor <victor@sunfox.ee>
2024-05-31 11:52:09 +0300

    Member joining script update.

--- commands/help.js
+++ commands/help.js
@@ -24,14 +24,36 @@ module.exports = {
 		subcommand
 			.setName('club')
 			.setDescription('Уведомления для группы Viru Vikings | Viru Vikings club notifications')
+    )
+	.addSubcommand(subcommand =>
+		subcommand
+			.setName('invite')
+			.setDescription('Создание приглашения на сервер | Create an invitation to the server')
     ),
 
     async execute(interaction) {
         if (interaction.options.getSubcommand() === 'play2gether') {
+            var HelpPlay2gether = new EmbedBuilder()
+                .setColor(config.colors.primaryDark)
+                .setTitle( "— Будем играть вместе!" )
+                .setDescription("Приглашай участников сообщества в кооперативные игры с помощью команды `/play2gether`. Доступно два режима использования команды: для экипажей Sea of Thieves, и для других игр.")
+                .setImage(config.ui.resourcesUrl + "/help/play2gether.gif")
+            .addFields(
+                { name: "/play2gether sot", value: "Выбери цель путешествия, тип судна и голосовой чат для общения с экипажем. После отправки команды, пираты Гильдии получат уведомление, и по возможности присоединятся к Тебе." },
+                { name: "/play2gether game", value: "Выбери игру из списка и голосовой чат для общения с командой. Участники сервера смогут подключиться к игровому лобби, воспользовавшись сгенерированной ссылкой-приглашением." },
+                { name: "\u200b", value: "Используй команду `/play2gether` как показано ниже, либо воспользуйся кнопками для быстрого создания лобби по шаблону." },
+            )
+            .setFooter({
+                icon_url: config.ui.icon_url,
+                text: config.ui.title
+            });
 
+            interaction.reply({ embeds: [HelpPlay2gether], ephemeral: true });
         } else if (interaction.options.getSubcommand() === 'profile') {
 
         } else if (interaction.options.getSubcommand() === 'club') {
+
+        } else if (interaction.options.getSubcommand() === 'invite') {
 
         }
     }
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -16,109 +16,47 @@ module.exports = {
         const UserNotifyChannel = member.client.channels.cache.get(config.log_channels.notifictions);
         const BotLogChannel = member.client.channels.cache.get(config.log_channels.log);
 
-        /*
-        * Step 1. Get user invite code
+        /* Step 1.
+        *  Select member data out from the
+        *  users table.
         */
-        member.guild.invites.fetch().then(newInvites => {
-            // This is the *existing* invites for the guild.
-            const oldInvites = invites.get(member.guild.id);
-            // Look through the invites, find the one for which the uses went up.
-            const invite = newInvites.find(i => i.uses > oldInvites.get(i.code));
-            // A real basic message with the information we need. 
-            BotLogChannel.send({ content: `JOIN: ${member.user} joined using invite code ${invite.code}.` });
-        });
-
-        /*
-        * Step 2. Get invite_id and add user_discord_uid to database.
-        */
-        getInviteId(invite.code, function (error, invite_id) {
-            if (error) {
-                BotLogChannel.send({ content: `JOIN: Invitation code ${invite.code} cannot be found in a database. User ${member.user} has been joined without going through Reineke.guildMemberAdd flow.` });
+        let sql1 = "SELECT * FROM users WHERE user_discord_uid = ? LIMIT 1;";
+        database.query(sql1, [member.user.id], (error1, user_data) => {
+            if (error1) {
+                BotLogChannel.send({ content: `[INVITE] JOIN ERROR: Can't get user ${member.user} data. Database error.` });
+                return;
+            } else if (result1.length != 0) {
+                // User is not registered in the database
+                BotLogChannel.send({ content: `[INVITE] JOIN ERROR: Can't get user ${member.user} data. User not found or there are dublicated records in DB` });
+                return;
             } else {
-                updateInvitedUser(invite_id, member.user.id, function (error) {
-                    if (error) {
-                        BotLogChannel.send({ content: `JOIN: Can't update ${member.user} record in a database. User has been joined without going through Reineke.guildMemberAdd flow.` });
-                    } else {
-                        /*
-                        * Step 3. Add a role to user
-                        */
-                        member.roles.add(config.roles.newbie);
+                /* Step 2
+                *  Assign default role to user
+                */
+                member.roles.add(config.roles.newbie);
 
-                        /* 
-                        * Step 4. Get user data and show self-introduction text
-                        */
-                        getInvitedUserData(invite.code, function (error, user_data) {
-                            if (error) {
-                                BotLogChannel.send({ content: `JOIN: Can't get user ${member.user} data. User has been joined without going through Reineke.guildMemberAdd flow.` });
-                            } else {
+                /* Step 3
+                *  Assign config.roles.community role to user
+                *  regarding the user_data.user_landing value
+                */
+                var communityRole = config.roles.community.find(communityRole => communityRole.key === user_data.user_landing);
+                if (communityRole) {
+                    member.roles.add(communityRole.id);
+                }
 
-                                var embed_aboutme = {
-                                    title: `Привет! Меня зовут ${user_data.user_name}`,
-                                    description: user_data.invite_user_story,
-                                    color: config.colors.primaryBright,
-                                    thumbnail: {
-                                        url: "https://cdn.discordapp.com/avatars/" + member.user.id + "/" + member.user.avatar + ".png"
-                                    },
-                                    timestamp: new Date().toISOString(),
-                                    footer: {
-                                        icon_url: `${config.url.resourcesUrl}/reineke/ico/reineke_logo.png`,
-                                        text: "Reineke"
-                                    }
-                                }
-                                UserNotifyChannel.send({ content: `К нам пришел ${member.user}, попривествуйте новичка! Кстати, он немного рассказал о себе:`, embeds: [embed_aboutme] });
-                            }
-                        });
-                    }
-                });
+                /* Step 4
+                 * Change user Nicename on the server regarding
+                 * the user_data.user_name value
+                 * and communityRole value
+                 */
+
+                if (user_data.user_name) {
+                    member.setNickname(user_data.user_name);
+                }
+
+                BotLogChannel.send({ content: `[INVITE] JOIN: User <@${member.user.id}> has been joined to the server. Default roles has been assigned.` });
+                return;
             }
         });
     },
 };
-
-getInviteId = function (invite_code, callback) {
-    // Prepare MySQL request to retrieve user data	
-    let sql_join_1 = "SELECT invite_id FROM invites WHERE invite_code = ? LIMIT 1;";
-    database.query(sql_join_1, [invite_code], (error_join_1, result_invite, fields) => {
-        if (error_join_1) {
-            callback("Database error.", null);
-            return;
-        } else if (result_invite.length == 0 || result_invite.length > 1) {
-            callback("Ошибка получения идентификатора приглашения.", null);
-            return;
-        } else {
-            callback(null, result_invite[0]);
-        }
-    });
-    // getInviteId closed
-}
-
-updateInvitedUser = function (user_invite_id, user_discord_uid, callback) {
-    let sql_join_2 = "UPDATE users SET user_discord_uid = ? WHERE user_invite_id = ?;";
-    database.query(sql_join_2, [user_discord_uid, user_invite_id], (error_join_2, pingback) => {
-        if (error_join_2) {
-            callback("Ошибка регистрации приглашения пользователя.");
-            return;
-        }
-        callback(null);
-    });
-    // updateInvitedUser closed
-}
-
-/*
-* Get user data from tables invites & users
-*/
-getInvitedUserData = function (user_invite_code, callback) {
-    let sql_join_3 = "SELECT invites.invite_user_story, users.user_name, users.user_discord_uid FROM invites LEFT JOIN users ON invites.invite_id = users.user_invite_id WHERE invites.invite_code=?;";
-    database.query(sql_join_3, [user_invite_code], (error_join_3, result_user, fields) => {
-        if (error_join_3) {
-            callback("Ошибка получения масссива данных пользователя (DB error).", null);
-            return;
-        } else if (result_user.length == 0 || result_user.length > 1) {
-            callback("Ошибка получения масссива данных пользователя (<>1).", null);
-            return;
-        } else {
-            callback(null, result_user[0]);
-        }
-    });
-    // getInvitedUserData closed
-}
