74529f1c1a4c5c31df62f70f471273e0c1866ac5
Vitgor <victor@sunfox.ee>
2024-09-15 10:02:24 +0300

    v1.3.1 - Add help command + cridentials generation for FleetCreator.

--- commands/play2gether.js
+++ commands/play2gether.js
@@ -226,8 +226,8 @@ async execute(interaction) {
 							.setDescription("Начало сессии - <t:" + time_to_go + ":R>")
 							.setThumbnail(config.url.resourcesUrl + "img/glitterbeard/" + img_ship_mission + ".png")
 							.addFields(
-								{ name: "Присоединяйся к рейду!", value: "Для участия в рейде Тебе потребуется установленный и настроенный **VPN**. Участникам сообщества Sunfox.ee бесплатно предоставляется безопасный VPN-сервис: обратись к Хранителям для получения более подробной информации." },
-								{ name: "Подготовка к рейду:", value: "[Установить и настроить VPN](https://wiki.sunfox.ee/glitterbeard:vpn)" },
+								{ name: "Присоединяйся к рейду!", value: "Для участия в рейде Тебе потребуется **VPN** и **FleetCreator**. Участникам сообщества Sunfox.ee за пределами РФ бесплатно предоставляется безопасный VPN-сервис: обратись к Хранителям для получения более подробной информации." },
+								{ name: "Подготовка к рейду:", value: "Установить FleetCreator c помощью </help fleetcreator:1284770916798169121>\n[Установить и настроить Sunfox.ee VPN](https://wiki.sunfox.ee/glitterbeard:vpn_eu) _или_\n[Установить и настроить WireGuard + FineVPN](https://wiki.sunfox.ee/glitterbeard:vpn_wireguard)" },
                                 { name: "Корабль:", value: text_ship_type },
                                 { name: "\u200b", value: "**Добавляйся в голосовой канал:**" },
                                 { name: "<#" + party_channel + ">", value: "\u200b" }
