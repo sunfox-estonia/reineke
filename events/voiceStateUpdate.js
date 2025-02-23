1995bf01a301fd25eccd7156d3c6450433d5e604
Vitgor <victor@sunfox.ee>
2025-02-23 16:22:21 +0200

    v1.12 - Add log record when user leaves server.

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -10,7 +10,7 @@ module.exports = {
     async execute(oldStatus, newStatus) {
         if (oldStatus.channelId  === null && newStatus.channelId !== null) {
             var BotLogChannel = newStatus.guild.channels.cache.get(config.log_channels.log);
-            BotLogChannel.send({content: `[VC]: User <@${newStatus.id}> joined to channel <#${newStatus.channelId}>`});
+            BotLogChannel.send({content: `[VC]: User <@${newStatus.id}> joined channel <#${newStatus.channelId}>`});
         } else if (newStatus.channelId === null) {
             var BotLogChannel = oldStatus.guild.channels.cache.get(config.log_channels.log);
             BotLogChannel.send({content: `[VC]: User <@${oldStatus.id}> left channel <#${oldStatus.channelId}>`});
